1902a42d128de45413002df960143589a5ea3a2e
Ana Karoline Grativol <grativol97@gmail.com>
2025-04-16 23:23:32 -0300

    atualizando a aula 012

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 15
+console.log(`Você tem ${idade} anos`)
+if (idade < 16) { // < MENOR QUE 16
+    console.log('Não vota')
+} else if (idade < 18 || idade > 65){ // ACIMA DE 16 MENOR DO QUE 18 OU A IDADE ACIMA DE 65
+        console.log('Voto opcional')
+} else { // MAIOR QUE 18
+    console.log('Voto obrigatório')
+}
+    
