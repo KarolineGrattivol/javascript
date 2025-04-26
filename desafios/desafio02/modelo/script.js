ead8d7a7bc7ac5d149e10e6ff75097923e9a3d06
Ana Karoline Grativol <grativol97@gmail.com>
2025-04-25 23:40:47 -0300

    atualizaçao do desafio

---
+++ desafios/desafio02/modelo/script.js
@@ -0,0 +1,8 @@
+function analisar(){
+    let num = document.getElementById('txtn')
+    let ana = document.getElementById('selana')
+    if (num.value.length <= 1 || ana.value.length > 100 ){
+        ana.innerHTML = 
+    }
+
+}
