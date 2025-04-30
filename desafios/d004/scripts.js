b73fa47c5b3f8bb6219b0f714517dafda3d0984e
Ana Karoline Grativol <grativol97@gmail.com>
2025-04-30 17:16:22 -0300

    desafio 004

---
+++ desafios/d004/scripts.js
@@ -0,0 +1,8 @@
+function clicar(){
+    let prod = window.prompt('Que produto você está comprando?')
+    
+    let cus = Number(window.prompt(`Quanto custa o ${prod} que você está comprando?`))
+    let val = Number(window.prompt(`Qual foi o valor que você deu para pagar o ${prod}?`))
+    
+    window.alert(`Você comprou ${prod} que custou R$ ${cus}. Deu R$ ${val} em dinheiro e vai receber R$ ${val-cus} de troco.Volte sempre! `)
+}
